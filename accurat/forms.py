e041e4524f0c66ad3c03368cc0a196d2b569a420
Christian Federmann <cfedermann@dfki.de>
2012-09-10 19:42:41 +0100

    Implemented jQuery "magic" for language pair selection.

--- accurat/forms.py
+++ accurat/forms.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Project: ACCURAT Demo Translation Services
  Author: Christian Federmann <cfedermann@dfki.de>
@@ -6,13 +5,36 @@ Project: ACCURAT Demo Translation Services
 from django import forms
 
 LANGUAGE_PAIRS = (
-  ('1', u'English → German'),
+  'English,German,TypeA',
+  'English,German,TypeB',
+  'English,German,TypeC',
+  'French,German,TypeA',
+  'French,German,TypeB',
+  'English,French,TypeA',
 )
 
+def _compute_source_choices():
+    """Returns available source language choices."""
+    choices = list(set([x.split(',')[0] for x in LANGUAGE_PAIRS]))
+    choices.sort()
+    return tuple((x, x) for x in choices)
+
+def _compute_target_choices():
+    """Returns available target language choices."""
+    choices = list(set([x.split(',')[1] for x in LANGUAGE_PAIRS]))
+    choices.sort()
+    return tuple((x, x) for x in choices)
+
+def _compute_type_choices():
+    """Returns available system type choices."""
+    choices = list(set([x.split(',')[2] for x in LANGUAGE_PAIRS]))
+    choices.sort()
+    return tuple((x, x) for x in choices)
+
 class TranslateForm(forms.Form):
-    source_language = forms.ChoiceField()
-    target_language = forms.ChoiceField()
-    system_type = forms.ChoiceField()
+    source_language = forms.ChoiceField(choices=_compute_source_choices())
+    target_language = forms.ChoiceField(choices=_compute_target_choices())
+    system_type = forms.ChoiceField(choices=_compute_type_choices())
     source_text = forms.CharField(max_length=1000,
       help_text="You can enter up to 1,000 characters...",
       widget=forms.Textarea(attrs={'class': 'span8', 'maxlength': 1000}))
--- accurat/views.py
+++ accurat/views.py
@@ -2,13 +2,34 @@
 Project: ACCURAT Demo Translation Services
  Author: Christian Federmann <cfedermann@dfki.de>
 """
+from json import dumps
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login as _login, logout as _logout
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from accurat.forms import TranslateForm
+from accurat.forms import TranslateForm, LANGUAGE_PAIRS
 from accurat.settings import COMMIT_TAG
+
+def _compile_json_data(language_pairs):
+    """
+    Converts a tuple of language pair data into a 2-dimensional JSON map.
+    """
+    json_data = {}
+    for language_pair in language_pairs:
+        items = language_pair.split(',')
+        _source = items[0].strip()
+        _target = items[1].strip()
+        _type =  items[2].strip()
+        if not _source in json_data.keys():
+            json_data[_source] = {}
+        
+        if not _target in json_data[_source].keys():
+            json_data[_source][_target] = []
+        
+        json_data[_source][_target].append(_type)
+    
+    return json_data
 
 def home(request):
     dictionary = {
@@ -20,25 +41,42 @@ def home(request):
 
 # @login_required
 def translate(request):
+    selected = ',,'
     if request.method == "POST":
         form = TranslateForm(request.POST)
         result = None
+        
+        print request.POST
+        
+        print "form.is_valid() = ", form.is_valid()
 
         if form.is_valid():
             # Create translation, blocking until result is ready
             # Set result variable...
-            language = form.cleaned_data['language_pair']
-            result = "TRANSLATION_{}_WOULD_BE_AVAILABLE_HERE".format(language)
+            _source = form.cleaned_data['source_language']
+            _target = form.cleaned_data['target_language']
+            _type = form.cleaned_data['system_type']
+            selected = '{},{},{}'.format(_source, _target, _type)
+            result = "TRANSLATION_{}_WOULD_BE_AVAILABLE_HERE".format(selected)
 
     else:
         form = TranslateForm()
         result = None
+    
+    print "selected: {}".format(selected)
+    
+    from accurat.forms import _compute_source_choices, _compute_target_choices, _compute_type_choices
+    print _compute_source_choices()
+    print _compute_target_choices()
+    print _compute_type_choices()
 
     dictionary = {
       'title': 'ACCURAT Translation Services',
       'commit_tag': COMMIT_TAG,
       'form': form,
       'result': result,
+      'selected': selected,
+      'json_data': dumps(_compile_json_data(LANGUAGE_PAIRS)),
     }
     return render_to_response("translate.html", dictionary,
       context_instance=RequestContext(request))
